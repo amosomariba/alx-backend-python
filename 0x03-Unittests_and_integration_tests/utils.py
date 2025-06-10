a9175ff9e697d58a3e787928b0056845350ca36e
Amos Omariba <amomariba@gmail.com>
2025-06-10 17:05:51 +0300

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -9,13 +9,11 @@ from typing import (
     Dict,
     Callable,
 )
-
 __all__ = [
     "access_nested_map",
     "get_json",
     "memoize",
 ]
-
 def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
     """Access nested map with key path.
     Parameters
@@ -34,14 +32,11 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
         if not isinstance(nested_map, Mapping):
             raise KeyError(key)
         nested_map = nested_map[key]
-
     return nested_map
-
 def get_json(url: str) -> Dict:
     """Get JSON from remote URL."""
     response = requests.get(url)
     return response.json()
-
 def memoize(fn: Callable) -> Callable:
     """Decorator to memoize a method.
     Example
@@ -59,12 +54,10 @@ def memoize(fn: Callable) -> Callable:
     42
     """
     attr_name = "_{}".format(fn.__name__)
-
     @wraps(fn)
     def memoized(self):
         """ "memoized wraps"""
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
-
     return property(memoized)
