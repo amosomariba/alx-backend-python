17fcbc207720982e059b8324eac8458911693a40
Amos Omariba <amomariba@gmail.com>
2025-06-17 12:18:00 +0300

    Pagination and Filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -145,6 +145,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 from datetime import timedelta
